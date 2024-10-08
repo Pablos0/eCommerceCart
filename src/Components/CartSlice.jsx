7af6b8eac0d1b96a049b4de1715f142db7ddcecb
Pablos <pablo.salcedom@gmail.com>
2024-10-08 10:38:10 -0400

    creating functions in task 5

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,22 @@
 
-const CartSlice = ({
- 
+import { createSlice } from '@reduxjs/toolkit';
+
+const CartSlice = createSlice({ // this is initialize with @reduxjs/toolkit and react-redux
+    name: 'cart', // this represent the name of the slice
+    initialState, // an object representing the initial state of your slice
+    reducers: { // this contain the reducer objects
+        addItemToCart(state, action) { // add a new item to the cart
+            const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+            } else {
+                state.cartItems.push({ ...action.payload, quantity: 1 });
+            }
+        },
+    }
 });
+const initialState = {
+    cartItems: [],
+};
 
 
