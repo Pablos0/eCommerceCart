b51db9f8c492a3b26d4e611e43817808d4f1b9e2
Pablos <pablo.salcedom@gmail.com>
2024-10-09 11:54:21 -0400

    Leaving in task 7, adding const in ProductList

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -13,10 +13,44 @@ const CartSlice = createSlice({ // this is initialize with @reduxjs/toolkit and 
                 state.cartItems.push({ ...action.payload, quantity: 1 });
             }
         },
+
+        removeItemFromCart(state, action) {
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload); // update cartItems array by filtering out the item with the ID provided in action payload
+        },
+
+        clearCart(state) { // It takes state parameter to clear cart
+            state.cartItems = [];
+        },
+
+        increaseItemQuantity(state, action) {
+            const itemToIncrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToIncrease) {
+                itemToIncrease.quantity += 1;
+            }
+            /* state: represents the current state of the Redux store. 
+            action: This is an object that describes the action that occurred. Redux actions are plain JavaScript objects that must have a type property indicating the type of action being performed. */
+        },
+
+        decreaseItemQuantity(state, action) {
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload);
+            if (itemToDecrease && itemToDecrease.quantity > 1) {
+                itemToDecrease.quantity -= 1;
+            }
+        },
     }
 });
 const initialState = {
     cartItems: [],
 };
+
+export const {
+    addItemToCart,
+    removeItemFromCart,
+    clearCart,
+    increaseItemQuantity,
+    decreaseItemQuantity, 
+} = CartSlice.actions;
+
+export default CartSlice.reducer;
 
 
